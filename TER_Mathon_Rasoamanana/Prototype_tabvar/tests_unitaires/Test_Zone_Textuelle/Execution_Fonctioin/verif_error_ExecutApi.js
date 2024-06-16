258a85c267167661c8079af6adb720ec51a41ac9
NathanaelRR <nathanael.r.r@gmail.com>
2024-06-17 00:51:47 +0200

    Tests

---
+++ TER_Mathon_Rasoamanana/Prototype_tabvar/tests_unitaires/Test_Zone_Textuelle/Execution_Fonctioin/verif_error_ExecutApi.js
@@ -0,0 +1,134 @@
+// Fonction de simulation d'exécution
+function ExecutApi(command) {
+    console.log(`Execution de la commande : ${command}`);
+    
+        // Vérifie si le module tabvar existe
+        if (typeof tabvar === 'undefined') {
+            console.error("[1.6, Module tabvar inexistant.]");
+            return [1.6, "Module tabvar inexistant."];
+        }
+        
+        // Maintenant, continuez avec le reste du code d'ExecutApi
+        const parsedFunction = document.getElementById('command-input') ;
+    
+        if (parsedFunction) {
+            const { functionName, params } = parsedFunction;
+    
+            const checkParams = (expectedCount) => {
+                if (!params || params.length < expectedCount) {
+                    return `[1.1, Erreur: Paramètres manquants pour la fonction '${functionName}'.]`;
+                }
+                return null;
+            };
+
+        try {
+            let result1 = "";
+
+            switch (functionName) {
+                case "init":
+                    result1 = tabvar.init();
+                    break;
+                case "function_definition":
+                    if (checkParams(4)) {
+                        result1 = checkParams(4);
+                    } else {
+                        result1 = tabvar.function_definition(...params);
+                    }
+                    break;
+                case "validate":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.validate(...params);
+                    }
+                    break;
+                case "deriv":
+                    if (checkParams(3)) {
+                        result1 = checkParams(3);
+                    } else {
+                        result1 = tabvar.deriv(...params);
+                    }
+                    break;
+                case "compare_values":
+                    if (checkParams(3)) {
+                        result1 = checkParams(3);
+                    } else {
+                        result1 = tabvar.compare_values(...params);
+                    }
+                    break;
+                case "simplify":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.simplify(...params);
+                    }
+                    break;
+                case "develop":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.develop(...params);
+                    }
+                    break;
+                case "interval":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.interval(...params);
+                    }
+                    break;
+                case "suppose_var":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.suppose_var(...params);
+                    }
+                    break;
+                case "is_positive":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.is_positive(...params);
+                    }
+                    break;
+                case "print_env":
+                    if (checkParams(1)) {
+                        result1 = checkParams(1);
+                    } else {
+                        result1 = tabvar.print_env(...params);
+                    }
+                    break;
+                case "defined":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.defined(...params);
+                    }
+                    break;
+                case "suppose":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.suppose(...params);
+                    }
+                    break;
+                case "proof_assistant":
+                    if (checkParams(2)) {
+                        result1 = checkParams(2);
+                    } else {
+                        result1 = tabvar.proof_assistant(...params);
+                    }
+                    break;
+                default:
+                    result1 = `[1.1, Erreur: Nom de fonction invalide '${functionName}']`;
+            }
+            console.log(`Résultat de l'exécution : ${result1}`);
+
+        } catch (error) {
+            console.error("[1.9, Erreur interne : " + error.message + "]");
+        }
+        
+    } else {
+        console.error("[1.1, Erreur: Format de commande invalide]");
+    }
+}
--- TER_Mathon_Rasoamanana/Prototype_tabvar/tests_unitaires/Test_Zone_Textuelle/README.md
+++ TER_Mathon_Rasoamanana/Prototype_tabvar/tests_unitaires/Test_Zone_Textuelle/README.md
@@ -2,13 +2,58 @@
 
 Ce document présente les tests unitaires pour différentes fonctionnalités du logiciel. 
 
-## test_ligne_commande
+## Création_Nom_Fonction
 
-- **Identification**:
-- **Description**: 
+- **Identification**: Vérifier si le nom d'une fonction avec ses paramètres matche. 
+- **Description**: Si on entre une chaîne de caractère dans une zone de texte on veut qu'il soit considérer comme une fonction. 
 - **Contraintes**: 
 - **Dépendances**:  
 - **Procédure de Test**:
-  1. 
-  2. 
+  1. Ouvrir le fichier .html 
+  2. Ouvrir la console du navigateur pour voir les retours
+  3. Taper une chaîne de caractère dans la zone de texte
+  4. Il suffit que la chaîne ait une parenthèse avec ou sans éléments 
+
+## Execution_Fonction
+
+- **Identification**: Vérifier si la fonction saisie est executée. 
+- **Description**: On souhaite que si la fonction saisie est une fonction du module tabvar avec les bon paramètres, alors elle est exécutée. 
+- **Contraintes**: Doit utiliser le module tabvar. Doit utiliser la fonction de création de nom de fonction 'parseFunctionStr(). 
+- **Dépendances**: Module tabvar.  
+- **Procédure de Test**:
+  1. Ouvrir le fichier .html 
+  2. Ouvrir la console du navigateur pour voir les retours
+  3. Taper une chaîne de caractère dans la zone de texte
+  4. Il suffit que la chaîne ait une parenthèse avec ou sans éléments 
+  5. Tests possibles:
+     **Commande valide**
+	ExecutApi("init()");
+     **Commande avec paramètres manquants**
+	ExecutApi("validate(param1)");
+     **Commande avec fonction non reconnue**
+	ExecutApi("unknown_function()");
+     **Commande avec erreur interne (simulée par une fonction non définie)**
+	ExecutApi("function_definition(param1, param2, param3, param4)");
+     **Commande avec syntaxe invalide**
+	ExecutApi("invalid_syntax");
+     **Commande avec paramètres manquants pour une fonction non reconnue**
+	ExecutApi("unknown_function(param1)");
+     **Commande avec un nombre incorrect de paramètres**
+	ExecutApi("develop(param1)");
+     **Commande avec des paramètres vides**
+	ExecutApi("print_env()");	
+
+## test_ligne_commande
+
+- **Identification**: Vérifie si la création de nom de fonction et l'execution de la foncion matche.
+- **Description**: Quand la fonction est éxécutée, elle émet un retour dans la zone de texte à la 2e ligne. 
+- **Contraintes**: Une liste au préalable de fonctions est contenus dans une variable pour guide l'utilisation. 
+- **Dépendances**: Utilisation de bibliothèque pour auto-complétion de chaîne de caractères. Utilisation du module tabvar.
+- **Procédure de Test**:
+  1. Ouvrir le fichier .html 
+  2. Ouvrir la console du navigateur pour voir les retours
+  3. Taper une chaîne de caractère dans la zone de texte
+  4. Tester les proposition de fonction
+  5. Voir les différents retour possible directement dans la zone de texte
+
 ---
